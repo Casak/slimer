3064f36ae3ead1accab9a38e4819a1962346154d
Vadym Mashtabiei <casak123@gmail.com>
2015-09-11 14:45:53 +0300

    Fixed OnResume() bug Refactor

--- app/src/main/java/casak/ru/slimer/StartActivity.java
+++ app/src/main/java/casak/ru/slimer/StartActivity.java
@@ -1,30 +1,20 @@
 package casak.ru.slimer;
 
-
 import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import android.widget.FrameLayout;
 
 
-
-    // TODO Show a preview
-    // TODO Lock the screen
-    // TODO Unlock the screen
     // TODO Recieve a charge signal
 
 public class StartActivity extends Activity {
 
-    private final List blockedKeys = new ArrayList(Arrays.asList(KeyEvent.KEYCODE_VOLUME_DOWN, KeyEvent.KEYCODE_VOLUME_UP));
-    private static boolean UIFlag = false;
+    private Camera mCamera;
+    private CameraPreview mPreview;
     private final String TAG = "START_ACTIVITY";
 
     @Override
@@ -33,7 +23,32 @@ public class StartActivity extends Activity {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
         setContentView(R.layout.activity_fullscreen);
+        mCamera = getCameraInstance();
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(mCamera == null){
+            mCamera = getCameraInstance();
+            mPreview = new CameraPreview(this, mCamera);
+            FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+            preview.addView(mPreview);
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mPreview.getHolder().removeCallback(mPreview);
+            mCamera.release();
+            mCamera = null;
+        }
     }
 
     @Override
@@ -52,16 +67,14 @@ public class StartActivity extends Activity {
             return true;
         }
         else if(event.getKeyCode() == KeyEvent.KEYCODE_VOLUME_DOWN){
-            try
-            {
+            try{
                 String command;
                 command = "LD_LIBRARY_PATH=/vendor/lib:/system/lib am startservice -n com.android.systemui/.SystemUIService";
                 Process proc = Runtime.getRuntime().exec(new String[] { "su", "-c", command }, null);
                 proc.waitFor();
                 Log.i(TAG, "UI Enabled");
             }
-            catch (Exception e)
-            {
+            catch (Exception e){
                 // TODO Write an exception handler
             }
             return true;
@@ -71,6 +84,26 @@ public class StartActivity extends Activity {
         }
     }
 
+
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(1);
+        }
+        catch (Exception e){
+            //TODO Write an exception handler
+        }
+        return c;
+    }
+
+
+
+
+/*
+    public static boolean isConnected(Context context) {
+        Intent intent = context.registerReceiver(null, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1);
+        return plugged == BatteryManager.BATTERY_STATUS_CHARGING || plugged == BatteryManager.BATTERY_PLUGGED_USB;
+    }
+    */
 }
-
-
